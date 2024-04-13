cf9499beafb8d686e3b9f4eae867cc7e7ca03d1d
Reslashd <152208706+Reslashd@users.noreply.github.com>
2024-04-13 13:46:06 +0200

    Create C7E5.c

---
+++ C7E5.c
@@ -0,0 +1,43 @@
+/*
+ * C Programming - A Modern Approach 2nd Edition
+ * Chapter 7 Excercises by Reslashd (c)2024
+ * 
+ */
+ 
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+	int input = 0;
+	int value = 0;
+
+	printf("Enter a word: ");	
+	while((input = getchar()) != '\n'){
+		switch(toupper(input)){
+			case 'A': case 'E': case 'I': case 'L': case 'N': case 'O':
+			case 'R': case 'S': case 'T': case 'U':
+			value += 1;
+			break;
+			case 'D': case 'G':
+			value += 2;
+			break;   
+			case 'B': case 'C': case 'M': case 'P': 
+			value += 3;
+			break; 
+			case 'F': case 'H': case 'V': case 'W': case 'Y':
+			value += 4;
+			break;
+			case 'K': 
+			value += 5;
+			break;
+			case 'J': case 'X': 
+			value += 8;
+			break;
+			case 'Q': case 'Z': 
+			value += 10;
+			break;			
+		}
+	}
+	printf("Scrabble value: %d\n", value);
+	return 0;
+}
