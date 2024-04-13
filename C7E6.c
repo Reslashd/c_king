d25c49b8a854a7c3521b1efaea2589b4d91d8592
Reslashd <152208706+Reslashd@users.noreply.github.com>
2024-04-13 13:46:34 +0200

    Create C7E6.c

---
+++ C7E6.c
@@ -0,0 +1,18 @@
+/*
+ * C Programming - A Modern Approach 2nd Edition
+ * Chapter 7 Excercises by Reslashd (c)2024
+ * 
+ */
+ 
+#include <stdio.h>
+
+int main(void){
+	printf("Int: %lu\n", sizeof(int));
+	printf("Short: %lu\n", sizeof(short));
+	printf("Long: %lu\n", sizeof(long));
+	printf("Float: %lu\n", sizeof(float));
+	printf("Double: %lu\n", sizeof(double));
+	printf("Long Double: %lu\n", sizeof(long double));
+	
+	return 0;
+}
