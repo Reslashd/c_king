b13f052ea7fc7c5ee85ed0bfddf71de20e9c78a8
Reslashd <152208706+Reslashd@users.noreply.github.com>
2024-04-13 13:40:36 +0200

    Create C6E11.c

---
+++ C6E11.c
@@ -0,0 +1,27 @@
+/*
+ * C Programming - A Modern Approach 2nd Edition
+ * Chapter 6 Excercises by Reslashd (c)2024
+ * 
+ */
+ 
+#include <stdio.h>
+
+
+int main(void){
+	int n = 0;
+	float e = 0.0f;
+	float result = 0.0f;
+
+	printf("Enter a integer: ");
+	scanf("%d", &n);
+	
+	e = 1 + (1/((float)n));
+	result = e;
+	
+	for(int j = 1 ; j < n ; j++){
+		result = result * e;
+	}
+	
+	printf("Euler's number is %f\n", result);		
+	return 0;
+}
